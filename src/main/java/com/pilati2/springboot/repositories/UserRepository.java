949c961da95e167ec9da4b2ce3cbaae0d2b2a4ef
Gabriel Pilati <gabrielms1504@gmail.com>
2024-08-07 22:29:26 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/pilati2/springboot/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.pilati2.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pilati2.springboot.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
