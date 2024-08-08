d024e888bc6d0d31fa95987aa89b2cf4809d2041
Gabriel Pilati <gabrielms1504@gmail.com>
2024-08-07 23:20:39 -0300

    Service layer, component registration

--- src/main/java/com/pilati2/springboot/repositories/UserRepository.java
+++ src/main/java/com/pilati2/springboot/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.pilati2.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.pilati2.springboot.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
---
+++ src/main/java/com/pilati2/springboot/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pilati2.springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pilati2.springboot.entities.User;
+import com.pilati2.springboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
