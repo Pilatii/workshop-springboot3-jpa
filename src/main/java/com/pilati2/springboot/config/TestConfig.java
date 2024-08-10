e1ef876eb3c82ae36cc2b62ab56ea965b189cd45
Gabriel Pilati <gabrielms1504@gmail.com>
2024-08-09 21:41:46 -0300

    Payment, one-to-one association

--- src/main/java/com/pilati2/springboot/config/TestConfig.java
+++ src/main/java/com/pilati2/springboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pilati2.springboot.entities.Category;
 import com.pilati2.springboot.entities.Order;
 import com.pilati2.springboot.entities.OrderItem;
+import com.pilati2.springboot.entities.Payment;
 import com.pilati2.springboot.entities.Product;
 import com.pilati2.springboot.entities.User;
 import com.pilati2.springboot.entities.enums.OrderStatus;
@@ -68,8 +69,8 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
@@ -81,6 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
-
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
