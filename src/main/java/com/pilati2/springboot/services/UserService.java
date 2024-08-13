092e8e259595ca8435cb861165e7d70477a5f856
Gabriel Pilati <gabrielms1504@gmail.com>
2024-08-12 21:28:48 -0300

    Exception handling - update

--- src/main/java/com/pilati2/springboot/services/UserService.java
+++ src/main/java/com/pilati2/springboot/services/UserService.java
@@ -5,13 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.pilati2.springboot.entities.User;
 import com.pilati2.springboot.repositories.UserRepository;
 import com.pilati2.springboot.services.exceptions.DatabaseException;
 import com.pilati2.springboot.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -42,9 +43,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
